3475c7a7c03b354ab17c21d15e7183eb67ccc437
nurbeksakiev <sakin.snn@gmail.com>
2017-08-31 22:49:24 -0500

    added solution with a stack

--- LeetCode/042_Trapping_Rain_Water.py
+++ LeetCode/042_Trapping_Rain_Water.py
@@ -1,4 +1,25 @@
 class Solution(object):
+
+    def trap(self, height):
+        ans = 0
+        st = []
+        
+        current = 0;
+        
+        while current < len(height):
+            while len(st) > 0 and height[current] > height[st[-1]]:
+                top = st.pop()
+                if len(st) == 0:
+                    break
+                boundary = current - st[-1] - 1
+                minHeight = min(height[current], height[st[-1]]) - height[top]
+                ans += (boundary * minHeight)
+            st.append(current)
+            current += 1
+        return ans
+
+# Solution 2
+
     def trap(self, height):
         left, right = 0, len(height) - 1
         tot = 0

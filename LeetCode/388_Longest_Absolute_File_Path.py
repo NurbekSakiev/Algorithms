6bf9d469e8c72b333f2e71959909d4396e89bd7f
Mac <mac@Macde-MacBook-Pro.local>
2017-09-14 22:13:55 -0500

    more efficient solution added

--- LeetCode/388_Longest_Absolute_File_Path.py
+++ LeetCode/388_Longest_Absolute_File_Path.py
@@ -15,9 +15,22 @@ class Solution(object):
                 maxLen = max(curLen - 1, maxLen)
             stack.append(ln)
         return maxLen
-            
-        
         
     def countLevel(self, s):
         curr = s.replace("\t", "")
         return len(s) - len(curr)
+
+# Solution 2
+
+
+    def lengthLongestPath(self, input):
+        dic = {0:0}
+        maxLen = 0
+        for s in input.splitlines():
+            name = s.strip('\t')
+            depth = len(s) - len(name)
+            if '.' in s:
+                maxLen = max(maxLen, dic[depth] + len(name))
+            else:
+                dic[depth + 1] = dic[depth] + len(name) + 1
+        return maxLen

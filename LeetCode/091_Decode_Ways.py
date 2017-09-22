d77089eefc5ce1f0c0b7303b756e7c2c8422faac
Nurbek Sakiev <sakin.snn@gmail.com>
2017-09-22 18:41:41 -0500

    Given an encoded message containing digits, determine the total number of ways to decode it.

---
+++ LeetCode/091_Decode_Ways.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def numDecodings(self, s):
+        if len(s) == 0:
+            return 0
+        dp = [0 for i in range(len(s) + 1)]
+        dp[0] = 1
+        dp[1] = 1 if s[0] != '0' else 0
+        
+        for i in range(2, len(s) + 1):
+            if self.isValid(s[i-1:i]):
+                dp[i] += dp[i-1]
+            if self.isValid(s[i-2:i]):
+                dp[i] += dp[i-2]
+        return dp[-1]
+    
+    
+    def isValid (self, s):
+        if s[0] == "0":
+            return False
+        num = int(s)
+        if 1 <= num <= 26:
+            return True
